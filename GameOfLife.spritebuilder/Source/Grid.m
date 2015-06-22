2dcab729fe09c1243cbc83513d2920f0bc1fcbde
Marius Horga <mhorga@gmail.com>
2015-06-22 11:05:15 +0300

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -39,7 +39,7 @@ static const int GRID_COLUMNS = 10;
             creature.position = ccp(x, y);
             [self addChild:creature];
             _gridArray[i][j] = creature;
-            creature.isAlive = YES;
+            //creature.isAlive = YES;
             x+=_cellWidth;
         }
         y += _cellHeight;
