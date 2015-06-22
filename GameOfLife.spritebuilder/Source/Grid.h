9c34e0509c0944b007706f5c390d13132c501347
Marius Horga <mhorga@gmail.com>
2015-06-22 10:35:24 +0300

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  
+//
+//  Created by Marius Horga on 6/22/15.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  
+//
+//  Created by Marius Horga on 6/22/15.
+//
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
